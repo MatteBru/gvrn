d22e07ee701ae95e4d534d3af1b64c12022838d3
Dakota Lillie <dakota.lillie@icloud.com>
2017-11-14 15:27:38 -0500

    add representatives and states

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114191634) do
+ActiveRecord::Schema.define(version: 20171114202440) do
+
+  create_table "representatives", force: :cascade do |t|
+    t.string "first_name"
+    t.string "middle_name"
+    t.string "last_name"
+    t.string "image"
+    t.date "date_of_birth"
+    t.text "biography"
+    t.string "party"
+    t.date "start_date"
+    t.string "leadership_role"
+    t.string "twitter_account"
+    t.string "facebook_account"
+    t.string "youtube_account"
+    t.string "url"
+    t.string "contact_form"
+    t.boolean "in_office"
+    t.float "dw_nominate"
+    t.string "next_election"
+    t.integer "total_votes"
+    t.integer "missed_votes"
+    t.string "office"
+    t.string "phone"
+    t.string "votes_with_party_pct"
+    t.string "gender"
+    t.boolean "at_large"
+    t.integer "district_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "senators", force: :cascade do |t|
     t.string "first_name"
@@ -38,6 +68,13 @@ ActiveRecord::Schema.define(version: 20171114191634) do
     t.float "votes_with_party_pct"
     t.string "gender"
     t.integer "state_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "name"
+    t.string "abbreviation"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'HTTParty'
-require 'byebug'
-require 'nokogiri'
-
 PROPUBLICA_HEADERS = {"X-API-Key" => "Bi8jLFPUDEt743g2DnWFchat1WJPsYd5ainS1uD1"}
 WIKIMEDIA_HEADERS = {"Api-User-Agent" => "gvrn/0.1 (bewguy101@gmail.com)"}
 CURRENT_CONGRESS = "115"
+STATES = {"AL"=>"Alabama", "AK"=>"Alaska", "AZ"=>"Arizona", "AR"=>"Arkansas", "CA"=>"California", "CO"=>"Colorado", "CT"=>"Connecticut", "DE"=>"Delaware", "DC"=>"District of Columbia", "FL"=>"Florida", "GA"=>"Georgia", "HI"=>"Hawaii", "ID"=>"Idaho", "IL"=>"Illinois", "IN"=>"Indiana", "IA"=>"Iowa", "KS"=>"Kansas", "KY"=>"Kentucky", "LA"=>"Louisiana", "ME"=>"Maine", "MD"=>"Maryland", "MA"=>"Massachusetts", "MI"=>"Michigan", "MN"=>"Minnesota", "MS"=>"Mississippi", "MO"=>"Missouri", "MT"=>"Montana", "NE"=>"Nebraska", "NV"=>"Nevada", "NH"=>"New Hampshire", "NJ"=>"New Jersey", "NM"=>"New Mexico", "NY"=>"New York", "NC"=>"North Carolina", "ND"=>"North Dakota", "OH"=>"Ohio", "OK"=>"Oklahoma", "OR"=>"Oregon", "PA"=>"Pennsylvania", "RI"=>"Rhode Island", "SC"=>"South Carolina", "SD"=>"South Dakota", "TN"=>"Tennessee", "TX"=>"Texas", "UT"=>"Utah", "VT"=>"Vermont", "VA"=>"Virginia", "WA"=>"Washington", "WV"=>"West Virginia", "WI"=>"Wisconsin","WY"=>"Wyoming"}
+
+def create_states
+  STATES.each do |a, n|
+    State.find_or_create_by(abbreviation: a, name: n)
+  end
+end
 
 def get_start_date(first_name, last_name)
   result_hash = HTTParty.get(
@@ -22,7 +25,18 @@ def get_start_date(first_name, last_name)
   random_string = result_hash["query"]["pages"].keys[0]
   xml_data = result_hash["query"]["pages"][random_string]["revisions"][0]["parsetree"]
   xml_doc  = Nokogiri::XML(xml_data)
-  xml_doc.at('name:contains("term_start")').parent.children[2].children.text.strip
+  term_start = xml_doc.at('name:contains("term_start")')
+  if !term_start
+    result_hash = HTTParty.get(
+      "https://en.wikipedia.org/w/api.php?action=query&titles=#{first_name}%20#{last_name}%20(politician)&prop=revisions&rvprop=parsetree&format=json",
+      :headers => WIKIMEDIA_HEADERS
+    ).parsed_response
+    random_string = result_hash["query"]["pages"].keys[0]
+    xml_data = result_hash["query"]["pages"][random_string]["revisions"][0]["parsetree"]
+    xml_doc  = Nokogiri::XML(xml_data)
+    term_start = xml_doc.at('name:contains("term_start")')
+  end
+  start_date = term_start.parent.children[2].children.text.strip
 end
 
 def get_bio(first_name, last_name)
@@ -32,6 +46,14 @@ def get_bio(first_name, last_name)
   ).parsed_response
   random_string = result_hash["query"]["pages"].keys[0]
   bio = result_hash["query"]["pages"][random_string]["extract"]
+  if bio.start_with?("#{first_name} #{last_name} may refer to:")
+    result_hash = HTTParty.get(
+      "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=#{first_name}%20#{last_name}%20(politician)",
+      :headers => WIKIMEDIA_HEADERS
+    ).parsed_response
+    random_string = result_hash["query"]["pages"].keys[0]
+    bio = result_hash["query"]["pages"][random_string]["extract"]
+  end
   bio.gsub(/\(.*\) /, "")
 end
 
@@ -42,6 +64,15 @@ def get_image(first_name, last_name)
   ).parsed_response
   random_string = result_hash["query"]["pages"].keys[0]
   image_string = result_hash["query"]["pages"][random_string]["pageimage"]
+  # if at a disambiguation page, go to the politician's page
+  if !image_string
+    result_hash = HTTParty.get(
+      "https://en.wikipedia.org/w/api.php?action=query&titles=#{first_name}%20#{last_name}%20(politician)&prop=pageimages&format=json", 
+      :headers => WIKIMEDIA_HEADERS
+    ).parsed_response
+    random_string = result_hash["query"]["pages"].keys[0]
+    image_string = result_hash["query"]["pages"][random_string]["pageimage"]
+  end
   slugged_image_url = image_string.gsub("_", "%20").prepend("File:")
   get_image_from_slug(slugged_image_url)
 end
@@ -84,8 +115,37 @@ def create_senator(senator_hash)
     votes_with_party_pct: senator_hash["votes_with_party_pct"],
     gender: HTTParty.get(senator_hash["api_uri"], :headers => PROPUBLICA_HEADERS)["results"][0]["gender"]
   )
-  byebug
-  "end"
+end
+
+def create_representative(representative_hash)
+  rep_state = representative_hash["state"]
+  representative = Representative.new(
+    first_name: representative_hash["first_name"],
+    middle_name: representative_hash["middle_name"],
+    last_name: representative_hash["last_name"],
+    image: get_image(representative_hash["first_name"], representative_hash["last_name"]),
+    date_of_birth: Date.parse(representative_hash["date_of_birth"]),
+    biography: get_bio(representative_hash["first_name"], representative_hash["last_name"]),
+    party: representative_hash["party"],
+    start_date: Date.parse(get_start_date(representative_hash["first_name"], representative_hash["last_name"])),
+    leadership_role: representative_hash["leadership_role"],
+    twitter_account: representative_hash["twitter_account"],
+    facebook_account: representative_hash["facebook_account"],
+    youtube_account: representative_hash["youtube_account"],
+    url: representative_hash["url"],
+    contact_form: representative_hash["contact_form"],
+    in_office: representative_hash["in_office"],
+    dw_nominate: representative_hash["dw_nominate"],
+    next_election: representative_hash["next_election"],
+    total_votes: representative_hash["total_votes"],
+    missed_votes: representative_hash["missed_votes"],
+    office: representative_hash["office"],
+    phone: representative_hash["phone"],
+    at_large: representative_hash["at_large"],
+    district_id: nil,
+    votes_with_party_pct: representative_hash["votes_with_party_pct"],
+    gender: HTTParty.get(representative_hash["api_uri"], :headers => PROPUBLICA_HEADERS)["results"][0]["gender"]
+  )
 end
 
 def lookup_senators
@@ -93,4 +153,11 @@ def lookup_senators
   create_senator(senators.first)
 end
 
+def lookup_representatives
+  representatives = HTTParty.get("https://api.propublica.org/congress/v1/#{CURRENT_CONGRESS}/house/members.json", :headers => PROPUBLICA_HEADERS)["results"][0]["members"]
+  create_representative(representatives.first)
+end
+
+create_states
+lookup_representatives
 lookup_senators
