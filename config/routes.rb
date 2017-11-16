7b2fcdccb8fbcb4b669e9f5af8ab0f95034a139d
Dakota Lillie <dakota.lillie@icloud.com>
2017-11-15 21:01:26 -0500

    moar progress

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create, :destroy]
   get "/login", to: "sessions#login", as: "login"
   get "/signup", to: "users#new", as: "signup"
+  get "/search", to: "static#search", as: "search"
+  get "/search_results", to: "static#search_results", as: "search_results"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # resources :reps
