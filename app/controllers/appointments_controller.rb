ebe54ab4b8098080a576beeee17d5e5869d20490
MatteBru <mbruni616@gmail.com>
2017-11-17 11:25:13 -0500

    whoops I did it again. added appointment functionality to front end

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,14 +1,44 @@
 class AppointmentsController < ApplicationController
 
+  def time_helper
+    y = appointment_params["date(1i)"].to_i
+    mo = appointment_params["date(2i)"].to_i
+    d = appointment_params["date(3i)"].to_i
+    h = appointment_params["time(4i)"].to_i
+    m = appointment_params["time(5i)"].to_i
+    # byebug
+    DateTime.new(y, mo, d, h, m) + 5.hours
+  end
+
   def create
     # byebug
-    params[:congressperson_type] == "representative" ? congressperson = Representative.find(params[:congressperson_id]) : congressperson = Senator.find(params[:congressperson_id])
+    appointment_params[:congressperson_type] == "representative" ? congressperson = Representative.find(appointment_params[:congressperson_id]) : congressperson = Senator.find(appointment_params[:congressperson_id])
     time = Time.now
     user = User.find(session[:user_id])
-    time = params[:time] if params[:delay]
+    # byebug
+
+    if appointment_params[:delay]
+      time = time_helper
+      flash[:message] = "Call Scheduled for #{time_helper.in_time_zone("Eastern Time (US & Canada)").strftime("%A, %B %e at %l:%M %p")}"
+    else
+      flash[:message] = "Call Initiated"
+    end
     Appointment.create(user: user, time: time, congressperson: congressperson)
-    flash[:message] = "Call Initiated"
     redirect_to user_path(user)
   end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:congressperson_type, :congressperson_id, :delay, :"date(1i)", :"date(2i)", :"date(3i)", :"time(4i)", :"time(5i)")
+  end
+
+  # "date(2i)"=>"11",
+  # "date(3i)"=>"17",
+  # "time(1i)"=>"1",
+  # "time(2i)"=>"1",
+  # "time(3i)"=>"1",
+  # "time(4i)"=>"20",
+  # "time(5i)"=>"35")
 
 end
