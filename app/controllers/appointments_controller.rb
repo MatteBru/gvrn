cf022a0d83585d123a59609ccdb7f8bdeb59b0ce
MatteBru <mbruni616@gmail.com>
2017-11-17 10:29:04 -0500

    whoops... pls disregard

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,8 +1,12 @@
 class AppointmentsController < ApplicationController
 
   def create
-    user = User.find(session[:user_id])
+    # byebug
+    params[:congressperson_type] == "representative" ? congressperson = Representative.find(params[:congressperson_id]) : congressperson = Senator.find(params[:congressperson_id])
     time = Time.now
+    user = User.find(session[:user_id])
+    time = params[:time] if params[:delay]
+    Appointment.create(user: user, time: time, congressperson: congressperson)
     redirect_to user_path(user)
   end
 
