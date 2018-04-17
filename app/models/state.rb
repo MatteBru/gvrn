a08721908a2a7fcdf928b0feffc22cc7381c06d8
MatteBru <mbruni616@gmail.com>
2018-04-17 13:23:11 -0400

    removed DC from states in seed file, patched issue with missing seniority from propublica API, changed db names

--- app/models/state.rb
+++ app/models/state.rb
@@ -4,7 +4,7 @@ class State < ApplicationRecord
   has_many :representatives, through: :districts
 
   def junior_senator
-    self.senators.where(in_office: true, state_rank: "junior").first
+    self.senators.where(in_office: true, state_rank: "junior").first || self.senators.find{|s| s != self.senior_senator}
   end
 
   def senior_senator
