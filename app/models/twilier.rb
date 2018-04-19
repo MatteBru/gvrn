5e967487edd7edc48bdc79623deeb47368e834dd
MatteBru <mbruni616@gmail.com>
2018-04-19 12:15:58 -0400

    removed commented test code from Twilier

--- app/models/twilier.rb
+++ app/models/twilier.rb
@@ -1,11 +1,4 @@
 module Twilier
-  # require 'twilio-ruby'
-
-
-  # ACCOUNT_SID = "ACd0c6a3b407a9e2259ef612a46c38d005" # Your Account SID from www.twilio.com/console
-  # AUTH_TOKEN = "4d081ee4f9e900b2d98db3f1a1fd309d"   # Your Auth Token from www.twilio.com/console
-  # TWILIO_NUMBER = "+15623624876"
-  # BASE_URL = "https://gvrn.herokuapp.com"
 
   ACCOUNT_SID = ENV['twilio_sid'] # Your Account SID from www.twilio.com/console
   AUTH_TOKEN = ENV['twilio_auth']   # Your Auth Token from www.twilio.com/console
@@ -17,8 +10,8 @@ module Twilier
     @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
     message = @client.messages.create(
         body: "Hello from GVRN, your appointment with #{self.congressperson.first_name} #{self.congressperson.last_name} is confirmed for #{self.time.in_time_zone("Eastern Time (US & Canada)").strftime("%A, %B %e at %l:%M %p")}.",
-        to: self.user.phone,    # Replace with your phone number
-        from: TWILIO_NUMBER)  # Replace with your Twilio number
+        to: self.user.phone,
+        from: TWILIO_NUMBER) 
 
     puts message.sid
   end
@@ -27,9 +20,8 @@ module Twilier
     @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
     message = @client.messages.create(
         body: "Hello from GVRN, your appointment with #{self.congressperson.first_name} #{self.congressperson.last_name} is in 30 seconds",
-        to: self.user.phone,    # Replace with your phone number
-        from: TWILIO_NUMBER)  # Replace with your Twilio number
-
+        to: self.user.phone,
+        from: TWILIO_NUMBER)
     puts message.sid
   end
 
@@ -37,8 +29,8 @@ module Twilier
     @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
     call = @client.calls.create(
         url: "#{BASE_URL}/connect/#{self.id}",
-        to: self.user.phone,    # Replace with your phone number
-        from: TWILIO_NUMBER)  # Replace with your Twilio number
+        to: self.user.phone,
+        from: TWILIO_NUMBER)
   end
 
   def add_rep
@@ -49,24 +41,4 @@ module Twilier
     end
   end
 
-
-  # def test_message
-  #
-  #   @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
-  #   message = @client.messages.create(
-  #       body: "Hello from GVRN",
-  #       to: "+12032955525",    # Replace with your phone number
-  #       from: TWILIO_NUMBER)  # Replace with your Twilio number
-  #
-  #   puts message.sid
-  # end
-  #
-  # def test_call
-  #   @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
-  #   call = @client.calls.create(
-  #       url: "http://b7a343c4.ngrok.io/connect",
-  #       to: "+12032955525",    # Replace with your phone number
-  #       from: TWILIO_NUMBER)  # Replace with your Twilio number
-  #
-  # end
 end
